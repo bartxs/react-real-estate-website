795ffef613f1d37e0f8e332ad5c712bf5041c914
Bartosz Soltysik <b.soltysik90@gmail.com>
2021-10-05 21:35:11 +0200

    Navbar update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,36 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { menuData } from "../data/ManuData";
+
+const Nav = styled.nav`
+  height: 60px;
+  background: red;
+`;
+
+const Logo = styled(Link)`
+  color: #fff;
+`;
+
+const MenuBars = styled.i``;
+const NavMenu = styled.div``;
+const NavMenuLinks = styled(Link)``;
 
 function Navbar() {
-  return <></>;
+  return (
+    <Nav>
+      <Logo to="/">ELIX</Logo>
+      <MenuBars />
+      <NavMenu>
+        {menuData.map((item, index) => {
+          <NavMenuLinks to={item.link} key={index}>
+            {item.title}
+          </NavMenuLinks>;
+        })}
+      </NavMenu>
+      <h1>Navbar</h1>
+    </Nav>
+  );
 }
 
 export default Navbar;
