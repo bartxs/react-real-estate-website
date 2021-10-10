963c9e15c9d9fd36e0d33dd1c5f528eb490ec2cd
Bartosz Soltysik <b.soltysik90@gmail.com>
2021-10-10 19:35:14 +0200

    Hero section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import GlobalStyle from "./globalStyle";
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <GlobalStyle />
       <Navbar />
+      <Hero />
     </>
   );
 }
