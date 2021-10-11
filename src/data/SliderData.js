66dcb949a46029e2ed59f80279b65555f3a782aa
Bartosz Soltysik <b.soltysik90@gmail.com>
2021-10-11 19:51:29 +0200

    slider data

---
+++ src/data/SliderData.js
@@ -0,0 +1,34 @@
+export const SliderData = [
+  {
+    title: "Luxtry Villa in Bali, Indonesia",
+    price: "$4,280,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "House in San Jose, Costa Rica",
+    price: "$2,800,00",
+    path: "/homes",
+    label: "View Home",
+    image: ImageTwo,
+    alt: "House",
+  },
+  {
+    title: "House in Calabassas, California",
+    price: "$8,280,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageThree,
+    alt: "House",
+  },
+  {
+    title: "House in Houston, Texas",
+    price: "$6,280,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageFour,
+    alt: "House",
+  },
+];
