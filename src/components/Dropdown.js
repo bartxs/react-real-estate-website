5ac33911a85dfd93b001dbb9adcc6c8af8328cd2
Bartosz Soltysik <b.soltysik90@gmail.com>
2021-10-18 23:18:41 +0200

    icon style

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -20,7 +20,15 @@ const DropdownContainer = styled.div`
   top: 0;
 `;
 
-const Icon = styled.div``;
+const Icon = styled.div`
+  position: absolute;
+  top: 1.2rem;
+  right: 1.5rem;
+  background: transparent;
+  font-size: 2rem;
+  cursor: pointer;
+  outline: none;
+`;
 const CloseIcon = styled(FaTimes)``;
 const DropdownWrapper = styled.div``;
 const DropdownMenu = styled.div`
