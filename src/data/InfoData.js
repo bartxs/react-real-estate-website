5390b2636efcb04b675c7624b998fd0ab1be3f59
Bartosz Soltysik <b.soltysik90@gmail.com>
2021-10-22 19:48:03 +0200

    info data

---
+++ src/data/InfoData.js
@@ -0,0 +1,23 @@
+export const InfoData = {
+  heading: "Explore our beautiful homes",
+  paragraphOne:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, quibusdam facere omnis ipsum commodi laudantium. Quaerat ipsam architecto velit repudiandae!",
+  paragraphTwo:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, reprehenderit.",
+  buttonLabel: "View Homes",
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: "Modern Designs",
+  paragraphOne:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, quibusdam facere omnis ipsum commodi laudantium.",
+  paragraphTwo:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, reprehenderit.",
+  buttonLabel: "View Homes",
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
