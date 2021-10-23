936c151abdb3caf20d358d028ed84903ace5d881
Bartosz Soltysik <b.soltysik90@gmail.com>
2021-10-23 12:34:37 +0200

    info data update

--- src/data/InfoData.js
+++ src/data/InfoData.js
@@ -1,3 +1,6 @@
+import ImageOne from "../images/interior1.jpg";
+import ImageTwo from "../images/interior2.jpg";
+
 export const InfoData = {
   heading: "Explore our beautiful homes",
   paragraphOne:
